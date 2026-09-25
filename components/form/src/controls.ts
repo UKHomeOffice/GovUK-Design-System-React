0edbb1c9a3fb8a12c9fe9d79a72cc6f4a56a9f33
Henry Wilkinson-Todd <henry.wilkinson-todd@digital.homeoffice.gov.uk>
2026-09-25 12:09:00 +0100

    chore: remove non-gds components

--- components/form/src/controls.ts
+++
@@ -1,9 +0,0 @@
-import type { ComponentProps, ComponentType } from 'react';
-import type { ControlProps } from '@react-foundry/forms';
-
-import { withControl } from '@react-foundry/forms';
-import { SubmitButton as _Submit } from '@not-govuk/button';
-
-type _SubmitProps = ComponentProps<typeof _Submit>;
-
-export const Submit: ComponentType<_SubmitProps & ControlProps> = withControl(_Submit);
--- components/page/src/index.ts
+++
@@ -1,9 +0,0 @@
-import Page from './Page';
-
-export default Page;
-export { GovUKPage } from './GovUKPage';
-export { NotGovUKPage } from './NotGovUKPage';
-export * from './Page';
-
-export type { GovUKPageProps } from './GovUKPage';
-export type { NotGovUKPageProps } from './NotGovUKPage';
